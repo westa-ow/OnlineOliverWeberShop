0308999d66224c74573a15bac60c197c8a50d7c2
Illia Salohub <eramcheg@gmail.com>
2024-10-06 23:36:48 +0200

    Update: special customer check

--- shop/context_processor.py
+++ shop/context_processor.py
@@ -7,3 +7,10 @@ def user_role(request):
         user_info = get_user_info(request.user.email)
         role = user_info['role'] if user_info and 'role' in user_info else None
     return {'user_role': role}
+
+def user_is_special(request):
+    special = False
+    if request.user.is_authenticated:
+        user_info = get_user_info(request.user.email)
+        special = user_info['special_customer'] if user_info and 'special_customer' in user_info else False
+    return {'isSpecialCustomer': special}
