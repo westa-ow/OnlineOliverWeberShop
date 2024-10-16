f9aa9fd2581ce0f228a768e0d041bf7fa2f76b7b
Illia Salohub <eramcheg@gmail.com>
2024-10-16 22:05:19 +0200

    Update: I've added python-dotenv library for correct using of .env files

--- OnlineShop/settings.py
+++ OnlineShop/settings.py
@@ -12,12 +12,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 from django.utils.translation import gettext_lazy as _
-
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
