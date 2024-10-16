dbe61746c9989ef83ca4a3e89233d7e2623be5ad
Illia Salohub <eramcheg@gmail.com>
2024-10-16 22:10:06 +0200

    Update: Added .env file location

--- OnlineShop/settings.py
+++ OnlineShop/settings.py
@@ -16,8 +16,8 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-load_dotenv()
+dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
+load_dotenv(dotenv_path)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
