b0e379ee4848b2599061a56a77863f4447d28297
Illia Salohub <eramcheg@gmail.com>
2024-10-16 23:09:03 +0200

    Update: changed ensure anonym

--- shop/middleware/ensure_anon_session_middleware.py
+++ shop/middleware/ensure_anon_session_middleware.py
@@ -8,14 +8,20 @@ class EnsureAnonymousSessionMiddleware:
 
     def __call__(self, request):
         # Код, выполняемый на каждый запрос перед view
-
         response = self.get_response(request)
-
         # Код, выполняемый на каждый ответ после view
-
         return response
 
     def process_view(self, request, view_func, view_args, view_kwargs):
+        # Проверка: если это вебхук Stripe, мы не выполняем перенаправление
+        webhook_paths = [
+            reverse('stripe_webhook'),  # Убедитесь, что путь совпадает с вашим маршрутом вебхука
+        ]
+
+        if request.path in webhook_paths:
+            # Если это запрос на вебхук, не делаем перенаправление
+            return None
+
         # Проверяем, есть ли у пользователя сессионный ключ
         if not request.user.is_authenticated and not request.session.session_key:
             # Генерируем сессионный ключ
@@ -23,3 +29,5 @@ class EnsureAnonymousSessionMiddleware:
             # Проверяем, не находимся ли мы уже на главной странице, чтобы избежать бесконечного редиректа
             if request.path != reverse('home'):
                 return redirect('home')
+
+        return None
