031115b3a762d9936f1561bde20a8325bd14e074
Illia Salohub <eramcheg@gmail.com>
2024-11-03 16:53:40 +0100

    Update: English is default language fix 2

--- shop/middleware/DefaultLanguageMiddlware.py
+++ shop/middleware/DefaultLanguageMiddlware.py
@@ -1,7 +1,7 @@
 from django.utils import translation
 from django.conf import settings
 from django.shortcuts import redirect
-
+import re
 
 class DefaultLanguageMiddleware:
     def __init__(self, get_response):
@@ -9,7 +9,8 @@ class DefaultLanguageMiddleware:
 
     def __call__(self, request):
         # Если язык не установлен, принудительно установить 'gb'
-        if not request.path.startswith(f'/{settings.LANGUAGE_CODE}/'):
-            return redirect(f'/{settings.LANGUAGE_CODE}/')
+        if not re.match(r'^/(gb|de|it|fr|es|ru)/', request.path):
+            # Перенаправляем на gb только если язык не установлен в URL
+            return redirect(f'/gb{request.path}')
         response = self.get_response(request)
         return response
