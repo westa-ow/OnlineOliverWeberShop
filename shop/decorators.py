ec7f73d5f9a1b31755538e7dad0967bfd7a810b8
Illia Salohub <eramcheg@gmail.com>
2025-04-13 00:18:02 +0200

    Update: * Added new decorators not_logged_in and ratelimit decorator

--- shop/decorators.py
+++ shop/decorators.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponseForbidden, HttpResponseRedirect, JsonResponse
+from django.http import HttpResponseForbidden, HttpResponseRedirect, JsonResponse, HttpResponse
 from functools import wraps
 
+from django.shortcuts import redirect
 from django.urls import reverse
+from django_ratelimit.decorators import ratelimit
 
 
 def login_required_or_session(f):
@@ -13,6 +15,7 @@ def login_required_or_session(f):
             return JsonResponse({'status': 'error', 'message': 'Access denied'}, status=403)
     return wrapper
 
+
 def logout_required(function):
     def wrap(request, *args, **kwargs):
         if request.user.is_authenticated:
@@ -20,3 +23,27 @@ def logout_required(function):
         else:
             return function(request, *args, **kwargs)
     return wrap
+
+
+def not_logged_in(view_func):
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home')
+        return view_func(request, *args, **kwargs)
+    return _wrapped_view
+
+
+def ratelimit_with_logging(key, rate, block=False, **kwargs):
+    def decorator(fn):
+        @ratelimit(key=key, rate=rate, block=False, **kwargs)
+        def wrapper(request, *args, **inner_kwargs):
+            if getattr(request, 'limited', False):
+                import logging
+                logger = logging.getLogger(__name__)
+                logger.warning("Rate limit exceeded for IP: %s", request.META.get('REMOTE_ADDR'))
+                # We return code 429 - Too Many Requests
+                return HttpResponse("Too Many Requests", status=429)
+            return fn(request, *args, **inner_kwargs)
+        return wrapper
+    return decorator
